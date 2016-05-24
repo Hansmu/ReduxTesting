e8383c035324034bdde37dc1bfcafbf28baa4908
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2016-05-24 15:34:33 +0300

    Added comment box functionality and tests for it.

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -23,4 +23,25 @@ describe('CommentBox', () => {
     it('Has a button', () => {
         expect(component.find('button')).to.exist;
     });
+
+    // Can nest tests that are very similar to one another, in a describe block.
+    // Before each runs before every it
+    describe('Entering some text', () => {
+        // Add another beforeEach here to specify a beforeEach that would only be
+        // executed for the its that are in this block of describe.
+        beforeEach(() => {
+            //Find a textarea element. Simulate simulates an event, clicks on the element, types in text.
+            //Simulate a change to the textarea, fake enter a text 'new comment'.
+            component.find('textarea').simulate('change', 'new comment');
+        });
+
+        it('Shows that text in the textarea', () => {
+            expect(component.find('textarea')).to.have.value('new comment');
+        });
+
+        it('When submitted clears the input', () => {
+            component.simulate('submit');
+            expect(component.find('textarea')).to.have.value('');
+        });
+    });
 });
