b8fe8abe275550c34cebedc1571f952b48fc20a4
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2016-05-24 15:16:28 +0300

    Removed comments from app_test, added a comment box to the app and tested it.

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,36 +1,14 @@
-/** Tests the <App> component.
- *  Always ask yourself what do you actually want to test.
- *  What do we want to be so that another developer doesn't break it.
- *  Just text in there right now, so check that text is on the component.
- * **/
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-// The purpose of wrapping here is because then the tests can be run safely. If an error happens, then everything doesn't just die.
-// Use describe to group together similar tests.
-describe('App', () => { //The string's only importance is for the report. Just a tool to help you understand tests. The name doesn't have to be the same as component. But it's the best name.
-  // Use it to test a single attribute of a target.
-  it('Shows the correct text', () => { //Group one single test. I'm testing blablabla. The name here is also only used for the report.
-    // Use expect to make an assertion(belief that something is true) about a target(what we're testing).
-    //Create an instance of App
-    const component = renderComponent(App);
+describe('App', () => {
+  let component;
 
-    expect(component).to.contain('React simple starter');
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
 
-    /**
-     *       The thing we
-     *       want to make
-     *       an assert                 The value we
-     *       about                     expect
-     *        __|______                 _____|______
-     *        |        |               |            |
-     * except(component).to.have.class('comment-box');
-     *                   |____________|
-     *                         |
-     *                   Assert matcher-
-     *                   how to compare
-     *                   the  two given
-     *                   values
-     * **/
+  it('Shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 });
