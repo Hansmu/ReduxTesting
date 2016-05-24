d4b977bca0ac364e3beeba719f1a830ca4c27189
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2016-05-24 16:01:29 +0300

    Added comment list and its tests.

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,4 +1,5 @@
 import { renderComponent, expect } from '../test_helper';
+
 import App from '../../src/components/app';
 
 describe('App', () => {
@@ -10,5 +11,9 @@ describe('App', () => {
 
   it('Shows a comment box', () => {
     expect(component.find('.comment-box')).to.exist;
+  });
+
+  it('Shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
   });
 });
